1cee4cef8e70aaedf2478da64a20ccd4748c2afc
chalana151 <chalanagowda29@gmail.com>
2025-03-25 18:55:39 +0530

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,6 @@
+num=int(input("please enter a number:"))
+cube=num*num*num
+print("the cube of number is :",cube)
+num=int(input("please enter a number:"))
+square=num*num
+print("the suuare of number is: ",square)
