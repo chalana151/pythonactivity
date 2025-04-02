4913146761a439d78dff350c540bac91da1db3c6
chalana151 <chalanagowda29@gmail.com>
2025-04-02 19:25:21 +0530

    Add files via upload

---
+++ Largest.py
@@ -0,0 +1,11 @@
+def find_largest(lst):
+    if not lst:
+        return None
+    largest=lst[0]
+    for number in lst:
+        if number>largest:
+            largest=number
+            return largest
+        numbers=[10,20,5,8,25,3]
+        largest_number=find_largest(numbers)
+        print(largest_number)
